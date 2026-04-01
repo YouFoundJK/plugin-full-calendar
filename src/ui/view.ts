1b2d42b622b233d7efc1e91e45b5818d2f7d0b93
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2026-04-01 13:13:04 +0200

    feat(caldav): automatically get calendar colour and name

--- src/ui/view.ts
+++ src/ui/view.ts
@@ -541,9 +541,7 @@ export class CalendarView extends ItemView {
           if (isTimeline) {
             if (!this.timelineResources) {
               this.timelineResources = this.buildTimelineResources();
-              // @ts-expect-error FullCalendar typings don't include all scheduler options
               this.fullCalendarView?.setOption('resources', this.timelineResources);
-              // @ts-expect-error FullCalendar typings don't include all scheduler options
               this.fullCalendarView?.setOption('resourcesInitiallyExpanded', false);
             }
             this.addShadowEventsToView();
@@ -969,9 +967,7 @@ export class CalendarView extends ItemView {
       if (currentViewType.includes('resourceTimeline')) {
         if (!this.timelineResources) {
           this.timelineResources = this.buildTimelineResources();
-          // @ts-expect-error FullCalendar typings don't include all scheduler options
           this.fullCalendarView?.setOption('resources', this.timelineResources);
-          // @ts-expect-error FullCalendar typings don't include all scheduler options
           this.fullCalendarView?.setOption('resourcesInitiallyExpanded', false);
         }
         this.addShadowEventsToView();
